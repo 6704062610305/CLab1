ea36ff851324f3b72d956f1631d0094429b1202a
Thitaree Kaewsuwan <s6704062610305@email.kmutnb.ac.th>
2024-11-28 14:39:56 +0700

    ส่งครั้งที่ 3
    รูปแบบ n1 n2

--- lab1minx.c
+++ lab1minx.c
@@ -3,10 +3,7 @@
 int main() {
   int num1;
   int num2;
-  printf("n1=");
-  scanf("%d", &num1);
-  printf("n2=");
-  scanf("%d", &num2);
+  scanf("%d %d", &num1 , &num2);
   if (num1 > num2) {
   printf("%d greater than %d by %d ",num1,num2,num1-num2);
   } else {
